649419f94d0c39d994534e50b337ea459fd6e304
oh hyun keun <dhgusrms23@naver.com>
2025-12-29 14:47:54 +0900

    fix:sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,21 +3,22 @@ import { getArticlePosts } from "@features/notion/api";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl =
-    process.env.NEXT_PUBLIC_SITE_URL ||
-    process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-    "http://localhost:5170";
+    process.env.NEXT_PUBLIC_SITE_URL ??
+    (process.env.VERCEL_PROJECT_PRODUCTION_URL
+      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+      : "http://localhost:5170");
 
   // 기본 페이지
   const routes = ["", "/article"].map((route) => ({
     url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString(),
+    lastModified: new Date(),
   }));
 
-  // 블로그 포스트 동적 생성
+  // 블로그 포스트
   const posts = await getArticlePosts();
   const articleRoutes = posts.map((post) => ({
     url: `${baseUrl}/article/${post.pageId}`,
-    lastModified: new Date(post.createdTime).toISOString(),
+    lastModified: new Date(post.createdTime),
   }));
 
   return [...routes, ...articleRoutes];
