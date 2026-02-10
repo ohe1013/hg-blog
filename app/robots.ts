0339a402b79174f17a8a9c62adc60d03e8859fde
oh hyun keun <dhgusrms23@naver.com>
2026-02-10 13:23:33 +0900

    fix : robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,16 +1,16 @@
 import { MetadataRoute } from "next";
 
-const baseUrl =
-  process.env.NEXT_PUBLIC_SITE_URL ||
-  process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-  "http://localhost:5170";
+function getBaseUrl() {
+  if (process.env.NEXT_PUBLIC_SITE_URL) return process.env.NEXT_PUBLIC_SITE_URL;
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL)
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  return "http://localhost:5170";
+}
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = getBaseUrl();
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: { userAgent: "*", allow: "/" },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
